b7795b038a11dd12dc8aa73bba99605d4a9b0d9f
Eva <EvaneseExotica@gmail.com>
2019-05-20 04:19:45 -0400

    Attempting to make JsonParser work.

--- AttributeJSONParser.gd
+++ AttributeJSONParser.gd
@@ -1,8 +1,17 @@
-extends Object
+extends Node2D
 
 var attributeData = {}
+func _ready():
+  load_json()
 
-func load_json(json, type):
+func load_json():
   var file = File.new()
+  assert file.file_exists("res://StatData/CharAttributes.json")
   file.open("res://StatData/CharAttributes.json", file.READ)
-  attributeData = parse_json(file.get_as_text())
+  #var attributeData = JSON.parse(file.get_as_text())
+  var attributeData = parse_json(file.get_as_text())
+  #assert attributeData.size() > 0
+  print(attributeData)
+  for item in attributeData:
+    print(str(item))
+  return attributeData
