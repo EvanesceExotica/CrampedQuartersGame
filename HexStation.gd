78d55838d4d7b75436f979bbab94bb1cc33ab45d
Eva <EvaneseExotica@gmail.com>
2019-07-18 02:38:22 -0400

    Finished the clicking minigame

---
+++ HexStation.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var hexArray 
+var inactiveHexArray = []
+var currentNode
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	populateHexArray()
+	activateRandomNode()
+	for item in hexArray:
+		item.connect("minigameNodeClicked", self, "activateRandomNode")
+	pass # Replace with function body.
+
+
+func populateHexArray():
+	var testArray = get_children()
+	hexArray = testArray
+	# for child in testArray:
+	# 	if child is MinigameNode:
+	# 		hexArray.append(child)
+
+func activateRandomNode():
+	hexArray.erase(currentNode)
+	if(hexArray.size() > 0):
+		print("Clicked me!")
+		var size = hexArray.size()
+		print(str(size))
+		var randomIndex = randi()%size
+		var randomNode = hexArray[randomIndex]
+		print(str(randomNode.name))
+		randomNode.tweenTimerToZero()
+		inactiveHexArray.append(randomNode)
+		currentNode = randomNode
+		#hexArray.erase(randomNode)
+		print(str(hexArray.size()))
+
+
+func resetEveryNode():
+	for item in hexArray:
+		item.changeHexColor(Color.white)
+	populateHexArray()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- MinigameNode.gd
+++ MinigameNode.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+class_name MinigameNode
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,19 +9,38 @@ onready var hex = get_node("Hex")
 onready var tween = get_node("Tween")
 onready var clickSpace = get_node("ClickSpace")
 
+signal minigameNodeClicked
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tweenTimerToZero()
+	#tweenTimerToZero()
 	pass # Replace with function body.
 
 func changeHexColor(var color):
 	hex.modulate = color 
 
 func tweenTimerToZero():
-	tween.interpolate_property(countdownCircle, "value", 100, 0, 3.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(countdownCircle, "value", 100, 0, 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(hex, "modulate", Color.white, Color.red, 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(countdownCircle, "modulate", countdownCircle.modulate, Color.red, 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 
-func _on_ClickSpace_input_event():
-	print("YOYOYO")
+func _on_ClickSpace_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && event.pressed):
+		print("YOYOYO")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 	pass
+
+
+func _on_Hex_pressed():
+	print("Yoyoyo")
+	#tween.stop(countdownCircle, "value")
+	tween.stop_all()
+	changeHexColor(Color.aquamarine)
+	countdownCircle.value = 0
+	emit_signal("minigameNodeClicked")
+	pass # Replace with function body.
+
+
+func _on_Tween_tween_completed(object, key):
+	print("Game over")
+	pass # Replace with function body.
