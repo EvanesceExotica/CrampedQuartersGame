b4b3c177c34ffff82995400eb81763a27f061858
Eva <EvaneseExotica@gmail.com>
2019-09-16 12:32:58 -0400

    Trying to make the event parser work

--- Events/EventParser.gd
+++ Events/EventParser.gd
@@ -5,14 +5,27 @@ var eventData
 
 func load_json():
 	var file = File.new()
-	assert file.file_exists("res://StatData/CharacterAttributes.json")
-	file.open("res://StatData/CharacterAttributes.json", file.READ)
-	attributeData = parse_json(file.get_as_text())
-    return attributeData
+	assert file.file_exists( "res://Events/Events.json")
+	file.open("res://Events/Events.json", file.READ)
+	eventData = parse_json(file.get_as_text())
+    return eventData
     
 func sendEventSignals():
-	var eventDictionary  = {}
-	eventDictionary = attributeData[attributeName]
+	var eventArray  = []
+	eventArray = eventData[events]
     pass
 
+func checkScope(scope):
+	pass
 
+func checkRequirements():
+	pass
+
+func createEvent():
+	#here we add the event to the object and add the choices along with it, maybe have a countdown?
+	pass
+
+func checkActions(action):
+
+	
+	pass
