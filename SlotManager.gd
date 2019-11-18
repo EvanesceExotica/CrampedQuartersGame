9423b14c5affaec36e136034c49ca35f54dbc13d
Eva <EvaneseExotica@gmail.com>
2019-11-18 01:11:04 -0500

    I think I made the aura thing work properly. Was using the wrong instanced scene with the same name, and had to keep the array from overflowing backwards by not putting limits (greater than zero, less than size)

--- SlotManager.gd
+++ SlotManager.gd
@@ -5,7 +5,7 @@ export var slotArrangement = [] #this is left to right
 func _ready():
     if get_children().size() > 0:
         for slot in get_children():
-            slotArrangement.append()
+            slotArrangement.append(slot)
     SignalManager.connect("emittingAura", self, "applyToAdjacentSlots")
 
 func applyToAdjacentSlots(slot, attribute, howManyAdjacent):
@@ -13,10 +13,12 @@ func applyToAdjacentSlots(slot, attribute, howManyAdjacent):
     #this is for auras
 
     var originSlotIndex = slotArrangement.find(slot)
-    for i in range howManyAdjacent:
-        print(i)
-        slotArrangement[originSlotIndex+i].applyNewAttributeToSlot(attribute)
-        slotArrangement[originSlotIndex-i].applyNewAttributeToSlot(attribute)
+    for i in range(1, howManyAdjacent):
+        print("Index of slot is " + str(originSlotIndex) + " applying to slot " + str(originSlotIndex+1) + " and " + str(originSlotIndex-1))
+        if originSlotIndex+i < slotArrangement.size():
+            slotArrangement[originSlotIndex+i].applyNewAttributeToSlot(attribute)
+        if originSlotIndex-i >= 0:
+            slotArrangement[originSlotIndex-i].applyNewAttributeToSlot(attribute)
 
 func spreadToAdjacentSlots(originSlot):
     pass
