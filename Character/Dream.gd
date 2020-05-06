5501f4eeae1255e40c1f65bbf018b870083c6070
Eva <EvaneseExotica@gmail.com>
2020-05-05 21:55:00 -0400

    Making dreams fade in and rise. Now need to have nightmares and such.

--- Character/Dream.gd
+++ Character/Dream.gd
@@ -5,29 +5,74 @@ var desires = {} #things they actually want
 var nightmares = {} #bad dreams -- click to chase away? -- will cause sanity imrpovements
 onready var interactableObject = get_node("InteractableObject")
 
-var speed = 10.0
+var speed = 100.0
 var direction = Vector2(0, -1)
 
 enum Type{
-    random,
-    desire,
-    nightmare
+	random,
+	desire,
+	nightmare
 }
 var ourType
 
 func generateType():
-    pass
+	ourType = Type.values()[ randi()%Type.size() ]
+	if ourType == Type.random:
+		pass
+	elif ourType == Type.desire:
+		pass
+	elif ourType == Type.nightmare:
+		#maybe nightamres are hidden as desires until you hover over them? Then they start to grow and scream?
+		pass
 
 func RiseAndFade():
-    pass
+	pass
 
 func _physics_process(delta):
-    move
-    set_pos(get_pos() + dir * (speed * delta))
+	translate(direction * (speed * delta))
+	#set_pos(get_pos() + dir * (speed * delta))
 
 func _on_Area2D_mouse_entered():
-    interactableObject.handInZone = true
-    pass
+	interactableObject.handInZone = true
+	pass
 
 func _on_Area2D_mouse_exited():
-    pass
+	pass
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && event.pressed):
+		Pop()
+
+func Grow():
+	var startScale = self.scale
+	var endScale = Vector2(40, 40)
+	$Tween.interpolate_property(self, "scale", startScale, endScale, 10, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+	
+func FadeIn():
+	$Tween.interpolate_property(self, "modulate", Color(1, 1, 1, 0), Color(1,1, 1, 1), 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+
+func FadeOut():
+	$Tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1,1, 1, 0), 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+
+func FadeToBlue():
+	$Tween.interpolate_property(self, "self_modulate", Color(1, 1, 1, 1), Color.cornflower, 5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+
+func FadeToRed():
+	$Tween.interpolate_property(self, "self_modulate", Color(1, 1, 1, 1), Color.red, 5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+
+func _ready():
+	#set to invisible
+	self.modulate = Color(1, 1, 1, 0)
+	FadeIn()
+	FadeToRed()
+	#FadeToBlue()
+	#Grow()
+
+func Pop():
+	$AnimationPlayer.play("Pop")
+	$Tween.stop_all()
--- DreamSpawner.gd
+++ DreamSpawner.gd
@@ -1,9 +1,47 @@
 extends Node2D
 
+var dreamTemplate = preload("res://Character/Dream.tscn")
 var dreams = {}
+onready var area2d = get_node("Area2D")
+onready var colshape2d = area2d.get_node("CollisionShape2D")
+
+func _ready():
+	$Timer.connect("timeout", self, "spawnDreams")
+	timeDreamSpawn()
 
 func spawnDreams():
-    pass
+	var centerpos = colshape2d.position + area2d.position
+	var size = colshape2d.shape.extents*2
+
+	randomize()
 
 
+	var dream = dreamTemplate.instance()
+		#initialize the location
+	
+	var spawnPosition = Vector2(0, 0)
+	#var testX = randi() % int(size.x)
+
+	var lowestPositionX = int(size.x/2) * area2d.global_scale.x
+	var lowestPositionY = int(size.y/2) * area2d.global_scale.y
+	var lowestPosition = Vector2(lowestPositionX, lowestPositionY)
+	
+	var highestPositionX = int(-size.x/2) * area2d.global_scale.x
+	var highestPositionY = int(-size.y/2) * area2d.global_scale.y
+	var highestPosition = Vector2(highestPositionX, highestPositionY)
+
+	spawnPosition.x = ((randi()% int(size.x) - (size.x/2)) * area2d.global_scale.x) + centerpos.x
+
+	spawnPosition.y = ((randi()% int(size.y) - (size.y/2)) * area2d.global_scale.y) + centerpos.y 
+
+		#find random position for location^
+
+		
+	dream.position = spawnPosition #spawnPositio
+
+	add_child(dream)
+
+func timeDreamSpawn():
+	$Timer.set_wait_time(2)
+	$Timer.start()
 
